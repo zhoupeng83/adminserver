be1809bebe4683833441fefa4a0706be11144f24
zhoupeng <2402111364@qq.com>
2022-06-23 16:08:15 +0800

    添加了一行

---
+++ src/main/java/com/zhoupeng/adminserver/Controller/UserController.java
@@ -0,0 +1,17 @@
+package com.zhoupeng.adminserver.Controller;
+
+/**
+ * @Author xiao
+ * @Create 2022/6/23 15:59
+ * @Desc
+ */
+public class UserController {
+
+public  void   show(){
+    System.out.println("zhoupeng");
+
+
+}
+
+
+}
